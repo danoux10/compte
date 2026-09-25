5368f4d720b0cb295d6030a97efb40a06401ab4f
Dany Barbe <63715196+danoux10@users.noreply.github.com>
2026-09-25 20:09:20 +0200

    update iconsCategories, quick log, add icons

--- Database/database.sql
+++ Database/database.sql
@@ -1,3 +1,4 @@
+DROP DATABASE IF EXISTS compte;
 CREATE DATABASE IF NOT EXISTS compte
     CHARACTER SET utf8mb4
     COLLATE utf8mb4_unicode_ci;
@@ -19,6 +20,7 @@ create table if not exists iconsCategories
 (
     idIconsCategories int unsigned not null primary key auto_increment,
     name              varchar(255) not null,
+    type              enum('income', 'expense') not null,
     created_at        timestamp default current_timestamp,
     updated_at        timestamp default current_timestamp on update current_timestamp
 ) ENGINE = InnoDB;
