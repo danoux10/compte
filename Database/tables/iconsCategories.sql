e658f5099a92822c93f4ac0c94a9f57fd5ba006a
Dany Barbe <63715196+danoux10@users.noreply.github.com>
2026-09-25 16:00:00 +0200

    updated database iconCategories

--- Database/tables/iconsCategories.sql
+++ Database/tables/iconsCategories.sql
@@ -1,6 +1,7 @@
-create table if not exists iconsCategories (
-    idIconsCategories int primary key auto_increment,
-    name varchar(255) not null,
-    created_at timestamp default current_timestamp,
-    updated_at timestamp default current_timestamp on update current_timestamp
-)ENGINE = InnoDB;
+create table if not exists iconsCategories
+(
+    idIconsCategories int unsigned not null primary key auto_increment,
+    name              varchar(255) not null,
+    created_at        timestamp default current_timestamp,
+    updated_at        timestamp default current_timestamp on update current_timestamp
+) ENGINE = InnoDB;
