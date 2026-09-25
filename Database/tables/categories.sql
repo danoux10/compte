bb3b4e5fca3c9d044cff14b3e67a14d95ee85a73
Dany Barbe <63715196+danoux10@users.noreply.github.com>
2026-09-25 15:27:30 +0200

    add icons table and update categories

--- Database/tables/categories.sql
+++ Database/tables/categories.sql
@@ -1,17 +1,22 @@
-CREATE TABLE categories
+CREATE TABLE IF NOT EXISTS categories
 (
     idCategory INT AUTO_INCREMENT PRIMARY KEY,
     name       VARCHAR(255)                NOT NULL,
     color      VARCHAR(7)                  NOT NULL,
-    icon       VARCHAR(255)                NOT NULL,
+    icon       INT UNSIGNED                NOT NULL,
     type       ENUM ('income', 'expense')  NOT NULL,
-    -- NULL = catégorie crée par défaut, sinon idUser = catégorie crée par l'utilisateur
-    idUser     INT UNSIGNED                NOT NULL DEFAULT NULL,
+    /* NULL = catégorie crée par défaut
+     sinon idUser = catégorie crée par l'utilisateur */
+    idUser     INT UNSIGNED                NULL DEFAULT NULL,
     state      ENUM ('active', 'inactive') NOT NULL DEFAULT 'active',
     created_at TIMESTAMP                            DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP                            DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    CONSTRAINT fk_categories_icon
+        FOREIGN KEY (icon)
+            REFERENCES icons (idIcon)
+            ON DELETE CASCADE,
     CONSTRAINT fk_categories_user
         FOREIGN KEY (idUser)
             REFERENCES users (idUser)
             ON DELETE CASCADE
-);
+)ENGINE = InnoDB;
