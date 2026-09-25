2c8376103d1ca2b237922c678f7d8c726886ee05
Dany Barbe <63715196+danoux10@users.noreply.github.com>
2026-09-25 15:55:29 +0200

    update database.sql

--- Database/database.sql
+++ Database/database.sql
@@ -6,30 +6,47 @@ use compte;
 
 CREATE TABLE IF NOT EXISTS users
 (
-    idUser   INT  UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name     VARCHAR(100) NOT NULL,
-    lastname VARCHAR(100) NOT NULL,
-    email    VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
+    idUser     INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name       VARCHAR(100) NOT NULL,
+    lastname   VARCHAR(100) NOT NULL,
+    email      VARCHAR(255) NOT NULL UNIQUE,
+    password   VARCHAR(255) NOT NULL,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-)ENGINE = InnoDB;
+) ENGINE = InnoDB;
+
+create table if not exists iconsCategories
+(
+    idIconsCategories int primary key auto_increment,
+    name              varchar(255) not null,
+    created_at        timestamp default current_timestamp,
+    updated_at        timestamp default current_timestamp on update current_timestamp
+) ENGINE = InnoDB;
 
 CREATE TABLE IF NOT EXISTS icons
 (
-    idIcon   INT  UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name     VARCHAR(100) NOT NULL,
-    path     VARCHAR(255) NOT NULL,
+    idIcon          INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name            VARCHAR(100) NOT NULL,
+    path            VARCHAR(255) NOT NULL,
+    /*
+     Chaque icône doit obligatoirement
+     appartenir à une catégorie.
+    */
+    iconCategory_id INT UNSIGNED NOT NULL,
     /* NULL = icon crée par défaut
     sinon idUser = icon ajouter par l'utilisateur */
-    user_id  INT UNSIGNED  NULL DEFAULT NULL,
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    user_id         INT UNSIGNED NULL DEFAULT NULL,
+    created_at      DATETIME          DEFAULT CURRENT_TIMESTAMP,
+    updated_at      DATETIME          DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    CONSTRAINT fk_icons_iconCategory
+        FOREIGN KEY (iconCategory_id)
+            REFERENCES iconsCategories (idIconsCategories)
+            ON DELETE CASCADE,
     CONSTRAINT fk_icons_user
         FOREIGN KEY (user_id)
             REFERENCES users (idUser)
             ON DELETE CASCADE
-)ENGINE = InnoDB;
+) ENGINE = InnoDB;
 
 CREATE TABLE IF NOT EXISTS categories
 (
@@ -40,7 +57,7 @@ CREATE TABLE IF NOT EXISTS categories
     type       ENUM ('income', 'expense')  NOT NULL,
     /* NULL = catégorie crée par défaut
      sinon idUser = catégorie crée par l'utilisateur */
-    idUser     INT UNSIGNED                NULL DEFAULT NULL,
+    idUser     INT UNSIGNED                NULL     DEFAULT NULL,
     state      ENUM ('active', 'inactive') NOT NULL DEFAULT 'active',
     created_at TIMESTAMP                            DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP                            DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
@@ -52,4 +69,4 @@ CREATE TABLE IF NOT EXISTS categories
         FOREIGN KEY (idUser)
             REFERENCES users (idUser)
             ON DELETE CASCADE
-)ENGINE = InnoDB;
+) ENGINE = InnoDB;
